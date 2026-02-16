98e732d75ceed3667be32750f3155fc3e880d609
paolo giacomin <giacominpaoolo@gmail.com>
2026-02-16 15:24:02 +0100

    Add files via upload

---
+++ src/EndPairing.h
@@ -0,0 +1,16 @@
+#ifndef EndPairing_h
+#define EndPairing_h
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+struct Arco {
+    int destinazione;
+    int peso;
+};
+
+void endPairing(int u);
+
+#endif
