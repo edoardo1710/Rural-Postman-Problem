10a7e6fd24a579c33cc2c57da4d41cb8e7402294
paolo giacomin <giacominpaoolo@gmail.com>
2026-03-04 09:14:16 +0100

    Refactor PROLOG example by removing unused arcs

--- PROLOG/ESEMPIO_2.pl
+++ PROLOG/ESEMPIO_2.pl
@@ -3,7 +3,6 @@ DATA BASE*/
 arco(a, b, 0).
 arco(a, c, 0).
 arco(a, d, 0).
-arco(b, c, 0).
 arco(b, d, 0).
 arco(c, b, 0).
 arco(d, a, 0).
@@ -22,4 +21,5 @@ cammina(NodoAttuale,NodoCorrente, ArchiDisponibili, [NodoAttuale|CamminoRestante
     cammina(NodoSuccessivo, NodoCorrente,ArchiRimasti, CamminoRestante, NuovoCosto, CostoTotale).
 
 connesso(X,arco(X,Y,W),Y,W).
+
 connesso(X,arco(Y,X,W),Y,W).
